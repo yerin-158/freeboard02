e74a5808c60b8af224dfbc2fbb7c2f3ccdcc78a9
robin.son <robin.son@kakaocorp.com>
2020-05-01 01:29:25 +0900

    YER-18 [api] 게시판 목록을 위한 데이터를 전달하는 API를 개발한다.

--- src/test/java/com/freeboard01/domain/board/boardRepositoryIntegrationTest.java
+++ src/test/java/com/freeboard01/domain/board/boardRepositoryIntegrationTest.java
@@ -7,10 +7,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/applicationContext.xml"})
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/applicationContext.xml"})
 @Transactional
 public class boardRepositoryIntegrationTest {
 
@@ -18,9 +20,13 @@ public class boardRepositoryIntegrationTest {
     private BoardRepository boardRepository = null;
 
     @Test
-    public void test1(){
-        BoardEntity entity = BoardEntity.builder().user("myName").password("1234").contents("test data~").build();
-        boardRepository.save(entity);
+    public void test1() {
+        List<BoardEntity> boardEntityList = new ArrayList<>();
+        for (int i = 0; i < 30; ++i) {
+            BoardEntity entity = BoardEntity.builder().user("myNameis" + i).title("제목입니다." + i + i + i).password("1234").contents("test data~" + i).build();
+            boardEntityList.add(entity);
+        }
+        boardRepository.saveAll(boardEntityList);
     }
 
 }
