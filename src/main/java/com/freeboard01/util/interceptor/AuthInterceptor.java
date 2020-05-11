b2d1dd134680617f97f774416d844d068453ba42
robin.son <robin.son@kakaocorp.com>
2020-05-11 16:21:27 +0900

    YER-39 [api] 로그인 세션 작업을 인터셉트로 위임한다.

--- src/main/java/com/freeboard01/util/interceptor/AuthInterceptor.java
+++ src/main/java/com/freeboard01/util/interceptor/AuthInterceptor.java
@@ -4,11 +4,14 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return super.preHandle(request, response, handler);
+        HttpSession httpSession = request.getSession();
+        return Optional.ofNullable(httpSession.getAttribute("USER")).isPresent();
     }
 }
