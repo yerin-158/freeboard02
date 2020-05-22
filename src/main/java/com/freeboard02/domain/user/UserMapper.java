41de0cb3d64b1ba0f05408d95c4f4f60e5272769
robin.son <robin.son@kakaocorp.com>
2020-05-22 16:00:30 +0900

    YER-57 [테스트] CRUD를 작성하고 테스트 코드를 수행한다.

---
+++ src/main/java/com/freeboard02/domain/user/UserMapper.java
@@ -0,0 +1,14 @@
+package com.freeboard02.domain.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    void save(UserEntity userEntity);
+
+    UserEntity findByAccountId(String accountId);
+
+    List<UserEntity> findByAccountIdLike(String target);
+}
